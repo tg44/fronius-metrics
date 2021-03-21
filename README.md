41574791adfa20bee0dbead70b75303e01d8d2c9
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2021-03-21 13:22:01 +0100

    added prometheus and ohmpilot

--- README.md
+++ README.md
@@ -1,36 +1,46 @@
-[![Docker Build Status](https://img.shields.io/docker/cloud/build/tg44/fronius-solar2mqtt?style=flat-square)](https://hub.docker.com/r/tg44/fronius-solar2mqtt)
+[![Docker Build Status](https://img.shields.io/docker/cloud/build/tg44/fronius-metrics?style=flat-square)](https://hub.docker.com/r/tg44/fronius-metrics)
 
-# fronius-solar2mqtt
+# fronius-metrics
 
-A small service to gather data from Fronius inverters an send them to MQTT.
+A small service to gather data from Fronius inverters/ohmpilot an send them to MQTT or export as prometheus metrics.
 
 ## Running the app
 
 ### Local install / dev
 You need node 12, start with `npm i` and then `node app.js`.
-For setting the MQTT you need to `export MQTT_URL=mqtt://localhost:1883` and optionally `export MQTT_USER="user" && export MQTT_PWD="pass" && MQTT_TOPIC=tele/solar` before the service start.
+
+For setting the MQTT you need to `export MQTT_URL=mqtt://localhost:1883` and `export MQTT_ENABLED=true` optionally `export MQTT_USER="user" && export MQTT_PWD="pass" && MQTT_TOPIC=tele/solar` before the service start.
+
+For setting the HTTP you need to `export HTTP_ENABLED=true`.
+
 Also you need to add `export INVERTER_HOST=""`.
 
 ### Docker and compose
 For docker you can run;
 ```
-docker run -e MQTT_URL="mqtt://localhost:1883" -e MQTT_TOPIC=tele/solar -e INVERTER_HOST="192.168.1.254" fronius-solar2mqtt
+docker run -e MQTT_ENABLED=true -e MQTT_URL="mqtt://localhost:1883" -e MQTT_TOPIC=tele/solar -e INVERTER_HOST="192.168.1.254" fronius-metrics
 ```
 For docker compose;
 ```
 version: '3.1'
 services:
   fronius-solar2mqtt:
-    image: tg44/fronius-solar2mqtt
+    image: tg44/fronius-metrics
     restart: unless-stopped
     environment:
-      - MQTT_URL=mqtt://localhost:1883
-      - INVERTER_HOST="192.168.1.254"
-      - MQTT_TOPIC=tele/solar
+      - INVERTER_HOST="192.168.1.254" #inverter ip, can be left out
+      - HEATER_HOST="192.168.1.253" #ohmpilot ip, can be left out
+      - REFRESH_RATE_SECONDS=30 #optionally you can override the api poll rate, default is 30
+      - MQTT_ENALED=true #enables mqtt default false
+      - MQTT_URL=mqtt://localhost:1883 #required for mqtt
+      - MQTT_TOPIC=tele/solar #required for mqtt
       - MQTT_USER=user #optional
       - MQTT_PWD=pass #optional
       - MQTT_CLIENT_ID=myFS2MQTT_client #optional
-      - REFRESH_RATE_SECONDS=30 #optionally you can override the api poll rate, default is 30
+      - HTTP_ENALED=true #enables prometheus default false
+      - HTTP_PORT=3000 #optional
+      - NODE_METRICS_PREFIX='' #optional
+      - METRICS_PREFIX=fronius_ #optional
 ```
 
 ## Support
--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const fronius = require('node-fronius-solar')
 //const util = require('util')
 const mqtt = require('mqtt')
+const client = require('prom-client')
+const Gauge = client.Gauge
+const express = require('express')
+const parser = require('fast-xml-parser');
+const he = require('he');
+const axios = require('axios')
 
+const mqttEnabled = process.env.MQTT_ENALED || false
 const mqttUrl = process.env.MQTT_URL || 'mqtt://localhost:1883'
 const mqttUser = process.env.MQTT_USER
 const mqttPassword = process.env.MQTT_PW
 const mqttClientId = process.env.MQTT_CLIENT_ID
 const mqttTopic = process.env.MQTT_TOPIC || 'tele/solar'
 
+const httpEnabled = process.env.HTTP_ENALED || false
+const port = process.env.HTTP_PORT || 3000
+const nodeMetricsPrefix = process.env.NODE_METRICS_PREFIX || ''
+const prefix = process.env.METRICS_PREFIX || 'fronius_'
+
 const refreshRateSeconds = process.env.REFRESH_RATE_SECONDS || 30
 const inverterHost = process.env.INVERTER_HOST
+const heaterHost = process.env.HEATER_HOST
 
 const inverterOptions = {
   host: inverterHost,
@@ -24,28 +37,149 @@ const additionalMqttParams = {
   password: mqttPassword
 }
 
-const client = mqtt.connect(mqttUrl, additionalMqttParams)
+const xmlParseOptions = {
+  attributeNamePrefix : "",
+  attrNodeName: false, //default is 'false'
+  textNodeName : "text",
+  ignoreAttributes : false,
+  ignoreNameSpace : true,
+  allowBooleanAttributes : false,
+  parseNodeValue : true,
+  parseAttributeValue : true,
+  trimValues: true,
+  cdataTagName: "__cdata", //default is 'false'
+  cdataPositionChar: "\\c",
+  parseTrueNumberOnly: false,
+  arrayMode: false, //"strict"
+  attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+  tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+  stopNodes: ["parse-me-as-string"]
+};
+
+let mqttClient = null
 
 console.info('App started')
 
-client.on('connect', () => {
-  console.info('MQTT connected to ' + mqttUrl)
-})
+if(mqttEnabled) {
+  mqttClient = mqtt.connect(mqttUrl, additionalMqttParams)
+  mqttClient.on('connect', () => {
+    console.info('MQTT connected to ' + mqttUrl)
+  })
+}
+
+let totalWatt = null
+let actualGrid = null
+let actualLoad = null
+let actualPV = null
+let heaterTemp = null
+let heaterHeatPower = null
+let heaterReglerOut = null
+
+if(httpEnabled) {
+  const app = express()
+
+  const collectDefaultMetrics = client.collectDefaultMetrics;
+  collectDefaultMetrics({prefix: nodeMetricsPrefix})
+
+  const register = client.register;
+
+  totalWatt = new Gauge({
+    name: prefix +'total_watt',
+    help: 'totalWatt',
+  });
+
+  actualGrid = new Gauge({
+    name: prefix +'actual_grid',
+    help: 'actualGrid',
+  });
+
+  actualLoad = new Gauge({
+    name: prefix +'actual_load',
+    help: 'actualLoad',
+  });
+
+  actualPV = new Gauge({
+    name: prefix +'actual_pv',
+    help: 'actualPV',
+  });
+
+  heaterTemp = new Gauge({
+    name: prefix +'heater_temp',
+    help: 'heaterTemp',
+  });
+
+  heaterHeatPower = new Gauge({
+    name: prefix +'heater_heat_power',
+    help: 'heaterHeatPower',
+  });
+
+  heaterReglerOut = new Gauge({
+    name: prefix +'heater_regler_out',
+    help: 'heaterReglerOut',
+  });
+
+  app.get('/', (req, res) => {
+    res.send('This is an empty index, you want to go to the <a href="/metrics">metrics</a> endpoint for data!')
+  })
+
+  app.get('/metrics', async (req, res) => {
+    try {
+      res.set('Content-Type', register.contentType);
+      res.end(await register.metrics());
+    } catch (ex) {
+      res.status(500).end(ex);
+    }
+  })
+
+  // Start the things
+  app.listen(port, () => {
+    console.log(`Exporter app listening at http://localhost:${port}`)
+  })
+}
+
 
 async function getData() {
-  const json = await fronius.GetPowerFlowRealtimeDataData(inverterOptions)
-  //console.log(util.inspect(json, {depth: 4, colors: true}));
+  let inverterStats = {}
+  if(inverterHost) {
+    const inverterJson = await fronius.GetPowerFlowRealtimeDataData(inverterOptions)
+    inverterStats = {
+      total_watt: inverterJson.Body.Data.Site.E_Total,
+      actual_grid: inverterJson.Body.Data.Site.P_Grid,
+      actual_load: inverterJson.Body.Data.Site.P_Load,
+      actual_pv: inverterJson.Body.Data.Site.P_PV,
+    }
+  }
+  let heaterstats = {}
+  if(heaterHost) {
+    const heaterResp = await axios.get("http://" + heaterHost + "/values.xml")
+    const heaterJson = parser.parse(heaterResp.data, xmlParseOptions);
+    heaterstats = {
+      heater_temp: +heaterJson.values.value.find(x => x.id === `valTemperatur`).text.split(" ")[0],
+      heater_heat_power: +heaterJson.values.value.find(x => x.id === `valHeatPower`).text.split(" ")[0],
+      heater_regler_out: +heaterJson.values.value.find(x => x.id === `valreglerout`).text.split(" ")[0],
+    }
+  }
   return {
-    total_watt: json.Body.Data.Site.E_Total,
-    actual_grid: json.Body.Data.Site.P_Grid,
-    actual_load: json.Body.Data.Site.P_Load,
-    actual_pv: json.Body.Data.Site.P_PV,
+    ...inverterStats,
+    ...heaterstats,
   }
 }
 
 async function getAndSend() {
   const data = await getData()
-  await client.publish(mqttTopic, JSON.stringify(data))
+  //console.log(data)
+  if(mqttEnabled) {
+    await mqttClient.publish(mqttTopic, JSON.stringify(data))
+  }
+  if(httpEnabled) {
+    if(inverterHost) totalWatt.set(data.total_watt)
+    if(inverterHost) actualGrid.set(data.actual_grid)
+    if(inverterHost) actualLoad.set(data.actual_load)
+    if(inverterHost) actualPV.set(data.actual_pv)
+    if(heaterHost) heaterHeatPower.set(data.heater_heat_power)
+    if(heaterHost) heaterTemp.set(data.heater_temp)
+    if(heaterHost) heaterReglerOut.set(data.heater_regler_out)
+  }
 }
 
 
